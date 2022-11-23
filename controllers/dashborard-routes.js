a29015492ce173ea8b457010ec2ceaf5abc459e8
Tahlia <tbolden13@gmail.com>
2022-11-23 15:46:11 -0800

    finishing controller folder

---
+++ controllers/dashborard-routes.js
@@ -0,0 +1,47 @@
+const { Post, User, Comment } = require('../models/');
+const router = require('express').Router();
+const withAuth = require('..utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            where: {
+                user_id: req.session.user_id,
+            },
+            include: [
+                {
+                    model: Comment,
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+        const posts = postData.map( (post) => post.get({ plain: true }));
+        console.log(posts);
+        res.render('dashboard', { posts, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.redirect('/');
+    }
+});
+
+router.get('/edit/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findbyPk(req.params.id);
+
+        if(postData) {
+            const post = postData.get({ post });
+        } else {
+            res.status(404).end();
+        }
+    } catch (err) {
+        res.redirect('login');
+    }
+});
+
+module.exports = router;
