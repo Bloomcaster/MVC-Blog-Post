87c2c54fe9f94e10f9f66bf51e1401f206b0908c
Tahlia <tbolden13@gmail.com>
2022-11-23 16:28:01 -0800

    finished homeroutes

---
+++ controllers/home-routes.js
@@ -0,0 +1,62 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('..models/');
+const withAuth = require('../utils/auth');
+
+//getting all posts for the homepage
+
+router.get('/', async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            uclude: [User],
+        });
+
+        const posts = postData.map( (post) => post.get({ plain: true }));
+
+        res.render('homepage', { posts, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//got single post
+router.get('/post/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findOne({
+            where: {
+                id: req.params.id,
+            },
+
+            include: [
+                {
+                    model: Comment,
+                    include: {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                },
+                {
+                    model: User,
+                    attributes: ['username'],
+                },
+            ],
+        });
+
+        if (postData) {
+            const post = postData.get({ plain: true });
+            console.log(post);
+            res.render('post-by-id', { post, logged_in: req.session.logged_in });
+        } else {
+            res.status(404).end();
+        }
+        
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//route for signup or login page
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+module.exports = router;
