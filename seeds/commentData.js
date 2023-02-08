8e4e5ac41299e87296712617a3d2e2deb0ec5693
Tahlia <tbolden13@gmail.com>
2023-02-07 17:35:15 -0800

    working on seed data index file

---
+++ seeds/commentData.js
@@ -0,0 +1,48 @@
+const { Comment } = require("../models");
+
+const commentData = [
+  {
+    comment_content: "Awesome!",
+    user_id: 2,
+    post_id: 3,
+  },
+  {
+    comment_content: "Keep the it coming!",
+    user_id: 3,
+    post_id: 3,
+  },
+  {
+    comment_content: "Let's work on this!",
+    user_id: 1,
+    post_id: 2,
+  },
+  {
+    comment_content: "Good job!",
+    user_id: 2,
+    post_id: 1,
+  },
+  {
+    comment_content: "This is exciting!",
+    user_id: 3,
+    post_id: 1,
+  },
+  {
+    comment_content: "Wonderful work.",
+    user_id: 3,
+    post_id: 2,
+  },
+  {
+    comment_content: "Nice!",
+    user_id: 2,
+    post_id: 3,
+  },
+  {
+    comment_content: "Keep the ideas coming!.",
+    user_id: 1,
+    post_id: 3,
+  },
+];
+
+const seedComments = () => Comment.bulkCreate(commentData);
+
+module.exports = seedComments;
