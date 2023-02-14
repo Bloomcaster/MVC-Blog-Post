517648629f6fec74e4db302f3b713642ec2a630d
Tahlia <tbolden13@gmail.com>
2023-02-13 17:06:07 -0800

    typo in routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const dashboardRoutes = require('./dashboard-routes.js');
-const homeRoutes = require('./home-routes.js');
+const homeRoutes = require('./homeRoutes.js');
 
 router.use('/api', apiRoutes);
 router.use('/dashboard', dashboardRoutes);
